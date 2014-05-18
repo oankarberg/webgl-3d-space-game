047fdac161ed4922788a2f691aa774b8abd77d6f
oankarberg <oankarberg@gmail.com>
2014-05-18 21:13:05 +0200

    kodstäd

--- GUI.js
+++ GUI.js
@@ -40,81 +40,67 @@ var activateControls = function(){
 
 function endGame(id, totalCoins){
 
-        // stop loop
-        window.cancelAnimationFrame(id);
+    // stop loop
+    window.cancelAnimationFrame(id);
 
-        $('#WebGL-container').css('opacity', '1').fadeTo(500, 0.8)
-        var time = $('#timer span').html();
+    $('#WebGL-container').css('opacity', '1').fadeTo(500, 0.8)
+    var time = $('#timer span').html();
 
-        // show results
-        $('#result #time').html(time);
-        $('#result #totalcoins').html(totalCoins);
+    // show results
+    $('#result #time').html(time);
+    $('#result #totalcoins').html(totalCoins);
 
-        $('#gameOverScreen').css('height', window.innerHeight/2);
-        $('#gameOverScreen').css('width', window.innerWidth/2);
-        $('#gameOverScreen').css('margin-top', window.innerHeight/4);
-        $('#gameOverScreen').css('margin-left', window.innerWidth/4);
-        $('#gameOverScreen').css('opacity', '0').fadeTo(500, 0.8)
+    $('#gameOverScreen').css('height', window.innerHeight/2);
+    $('#gameOverScreen').css('width', window.innerWidth/2);
+    $('#gameOverScreen').css('margin-top', window.innerHeight/4);
+    $('#gameOverScreen').css('margin-left', window.innerWidth/4);
+    $('#gameOverScreen').css('opacity', '0').fadeTo(500, 0.8)
 
-        //restart page
-        $('a').click(function(){
-           //sätter url till playagain
-            if(checkRefresh() != 'playagain'){
-                location.href = location.href+'?playagain'; 
-               console.log(location.href);
-                location.reload();
-            }else{
-                location.reload();
-                 console.log(location.href  +  "1");
-            }
-        });
+    //restart page
+    $('a').click(function(){
+       //sätter url till playagain
+        if(checkRefresh() != 'playagain'){
+            location.href = location.href+'?playagain'; 
+           console.log(location.href);
+            location.reload();
+        }else{
+            location.reload();
+             console.log(location.href  +  "1");
+        }
+    });
 
-        gameOver = true;
+    gameOver = true;
 
 }
 
 function startGameScreen(){
 
-        $('#WebGL-container').css('opacity', '1').fadeTo(2000, 0.8)
- 
-        $('#startGameScreen').css('height', window.innerHeight/1.8);
-        $('#startGameScreen').css('width', window.innerWidth/2);
-        $('#startGameScreen').css('margin-top', window.innerHeight/8);
-        $('#startGameScreen').css('margin-left', window.innerWidth/4);
-        $('#startGameScreen #options').css('width', window.innerWidth/3);
+    $('#WebGL-container').css('opacity', '1').fadeTo(2000, 0.8)
 
-        $('#startGameScreen').css('opacity', '0').fadeTo(2000, 0.8)
+    $('#startGameScreen').css('height', window.innerHeight/1.8);
+    $('#startGameScreen').css('width', window.innerWidth/2);
+    $('#startGameScreen').css('margin-top', window.innerHeight/8);
+    $('#startGameScreen').css('margin-left', window.innerWidth/4);
+    $('#startGameScreen #options').css('width', window.innerWidth/3);
 
-        //gameOver = true; 
+    $('#startGameScreen').css('opacity', '0').fadeTo(2000, 0.8)
+
+    //gameOver = true; 
 
 }
 
 
 //ta bort startscreen
 function playGame()
-    {
-        activateControls();
-        gameOver = false;
-        startTimer();
-         // stop loop
-      
-        $('#WebGL-container').css('opacity', '1').fadeTo(500, 1);
-        //var time = $('#timer span').html();
-
-        // show results
-       // $('#result #time').html(time);
-        //$('#result #totalcoins').html(totalCoins);
-
-/*      Behövs inte??
-        $('#startGameScreen').css('height', window.innerHeight/2);
-        $('#startGameScreen').css('width', window.innerWidth/2);
-        $('#startGameScreen').css('margin-top', window.innerHeight/4);
-        $('#startGameScreen').css('margin-left', window.innerWidth/4);
-        $('#startGameScreen #options').css('width', window.innerWidth/3);*/
-    
-        $('#startGameScreen').css('display', 'none');
-        //return false; varför return false funktion kallas aldrig på förutom i index?
-    }
+{
+    activateControls();
+    gameOver = false;
+    startTimer();
+     // stop loop
+    $('#WebGL-container').css('opacity', '1').fadeTo(500, 1);
+    $('#startGameScreen').css('display', 'none');
+       
+}
 
 
 /// FÖR TIMERN ////
